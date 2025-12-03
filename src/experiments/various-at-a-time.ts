ac49a0281f7e11a3b23f0ef512129f330ccd4c57
Dan Montgomery <dan@montgomery.onl>
2025-12-02 21:26:47 -0600

    Add at a time experiment.

--- src/experiments/various-at-a-time.ts
+++ src/experiments/various-at-a-time.ts
@@ -1,27 +1,15 @@
-import { shuffleRiffleWithAccuracyFunctionFactory, shuffleTrulyRandom } from "./shuffle.js"
-import { runTest, summarizeHandSuitDistribution, type HandDistributionSummary, type LabeledTest } from "./test.js"
-import { printTestsSummaryTable } from "./print.js";
+import { shuffleRiffleWithAccuracyFunctionFactory, shuffleTrulyRandom } from "../shuffle.js"
+import { runTest, summarizeHandSuitDistribution, type HandDistributionSummary, type LabeledTest } from "../test.js"
+import { printTestsSummaryTable } from "../print.js";
 
 // Deck composition
 const numberOfSamples = 10000
 const numberOfSuits = 4
 const numberOfRanks = 13
 const numberOfPlayers = 4
-const atATime = 1
+const riffleAccuracy = 0.8
 
-// Riffle Shuffle test.
-// console.log('Starting Deck', createOrderedDeck({ numberOfSuits, numberOfRanks }))
-// console.log('After 8 perfect riffle shuffles', shuffle({
-//   deck: createOrderedDeck({ numberOfSuits, numberOfRanks }),
-//   shufflingFunction: shuffleRiffleWithAccuracyFunctionFactory(1),
-//   shuffleCount: 8,
-// }))
-
-// Distribution of suits with various accuracies.
-
-for (let accuracy = 1; accuracy >= 0; accuracy -= 0.1) {
-  const roundedAccuracy = Math.round(accuracy * 10) / 10
-
+for (let atATime = 1; atATime <= 4; atATime += 1) {
   const tests: LabeledTest[]  = []
 
   // Add a truly random shuffle as a baseline.
@@ -41,13 +29,13 @@ for (let accuracy = 1; accuracy >= 0; accuracy -= 0.1) {
 
   for (let numberOfShuffles = 1; numberOfShuffles <= 8; numberOfShuffles++) {
     tests.push({
-      label: `Riffle (${roundedAccuracy.toFixed(1)}) x ${numberOfShuffles}`,
+      label: `Deal ${atATime}, ${numberOfShuffles} shuffles`,
       summary: runTest({
         numberOfSamples,
         numberOfPlayers,
         numberOfSuits,
         numberOfRanks,
-        shufflingFunction: shuffleRiffleWithAccuracyFunctionFactory(roundedAccuracy),
+        shufflingFunction: shuffleRiffleWithAccuracyFunctionFactory(riffleAccuracy),
         shuffleCount: numberOfShuffles,
         atATime,
         handSummarizationFunction: summarizeHandSuitDistribution,
